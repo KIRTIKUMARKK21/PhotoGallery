d7cb9b21e7b1bcf90cfff5902d09ae9426e8f9ca
shikhar13012001 <75368010+shikhar13012001@users.noreply.github.com>
2021-11-01 22:20:19 +0530

    fixed gallery

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import useFirestore from '../hooks/useFirestore';
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
+import ImageGallery from "react-image-gallery";
 // import { motion } from 'framer-motion';
-const ImageGrid=()=>{
-
-    // const {docs}=useFirestore('images');
-    const {docs}=useFirestore();
-    console.log(docs);
-    return(
-        <div className='img-grid'>
-          {docs && docs.map(doc =>(
-              <div className="imag-wrap" key={doc.id}>
-                  <img src={doc.downloadURL} alt="uploaded pic"/>
-              </div>    
-          ))}
-        </div>
-    )
-
-}
+const ImageGrid = () => {
+  // const {docs}=useFirestore('images');
+  const images = [];
+  const { docs } = useFirestore();
+  console.log(docs);
+  if (docs.length)
+    for (let i = 0; i < docs.length; i++) {
+      images.push({
+        original: docs[i].downloadURL,
+        thumbnail: docs[i].downloadURL,
+      });
+    }
+  console.log(images);
+  return docs.length>0?<ImageGallery items={images} thumbnailPosition="right" />:null;
+};
 export default ImageGrid;
